aab1a33e1e182a7558e1b110e11cdf9f56e231f2
we6jbo <we6jbo@gmail.com>
2013-08-27 00:50:14 -0700

    This basically lets me print the GPL3 onto the screen so that I can show it to people running the program and they'll know what kind of license this program is under. Signed-off-by:we6jbo <we6jbo@gmail.com>

---
+++ We6jboisFreeSL/src/main/java/gpl20/GPL20.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2013 Jeremiah ONeal <joneal@nuaitp.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package gpl20;
+
+/**
+ *
+ * @author Jeremiah ONeal <joneal@nuaitp.net>
+ */
+public class GPL20 {
+    GPL20(String platform)
+    {
+        say.Say.info("----","Copyright (C) 2013 Jeremiah ONeal <joneal@nuaitp.net>");
+        say.Say.info("----","");
+        say.Say.info("----","This program is free software; you can redistribute it and/or");
+        say.Say.info("----","modify it under the terms of the GNU General Public License");
+        say.Say.info("----","as published by the Free Software Foundation; either version 2");
+        say.Say.info("----","of the License, or (at your option) any later version.");
+        say.Say.info("----","");
+        say.Say.info("----","This program is distributed in the hope that it will be useful,");
+        say.Say.info("----","but WITHOUT ANY WARRANTY; without even the implied warranty of");
+        say.Say.info("----","MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the");
+        say.Say.info("----","GNU General Public License for more details.");
+        say.Say.info("----","");
+        say.Say.info("----","You should have received a copy of the GNU General Public License");
+        say.Say.info("----","along with this program; if not, write to the Free Software");
+        say.Say.info("----","Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.");
+
+    }
+    }
+}
